8ba62552dc400d0f15f5a9dbe70232385369f30d
Aubin Djou <116364607+acelest@users.noreply.github.com>
2024-05-31 19:40:07 +0100

    add assets for bg

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
 import { Metadata } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import { Gutter } from '../../_components/Gutter'
 import { RenderParams } from '../../_components/RenderParams'
 import { getMeUser } from '../../_utilities/getMeUser'
 import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
@@ -21,7 +19,7 @@ export default async function Login() {
       <div className={classes.heroImg}>
         <Link href="/">
           <Image
-            src="/logo-black.svg"
+            src="/logo-white.svg"
             alt="logo"
             width={250}
             height={23}
